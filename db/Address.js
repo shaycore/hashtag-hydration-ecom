5090f758cc426b827d3189f0763f5c19efe4fa1f
Paulina Cole <lina.cole@gmail.com>
2022-08-14 17:05:45 -0400

    updated address model

--- db/Address.js
+++ db/Address.js
@@ -1,12 +1,61 @@
 const conn = require('./conn');
 const { Sequelize } = conn;
-const { STRING } = Sequelize;
+const { STRING, INTEGER, ENUM } = Sequelize;
 
-const Address = conn.define('address', {
-    address: {
-        type: STRING,
-        isNull: false
+const countries = ["USA", "Canada", "Mexico"];
+
+const Address = conn.define("address", {
+  firstName: {
+    type: STRING,
+    validate: {
+      notEmpty: true
     }
+  },
+  lastName: {
+    type: STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  addressLine1: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  addressLine2: {
+    type: STRING,
+  },
+  city: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  state: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  zipCode: {
+    type: INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  country: {
+    type: ENUM(countries),
+    allowNull: false,
+    defaultValue: "USA",
+    validate: {
+      notEmpty: true,
+    },
+  },
 });
 
 module.exports = Address;
