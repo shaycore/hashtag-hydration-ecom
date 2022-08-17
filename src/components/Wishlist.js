60f2a364c13335d4b54afba62fe09d34aff119b4
Paulina Cole <lina.cole@gmail.com>
2022-08-16 21:46:41 -0400

    added wishlist component

---
+++ src/components/Wishlist.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class WishList extends React.Component {
+    render() {
+        return (
+            <div>
+                Wishlist
+            </div>
+        )
+    }
+
+}
+
+const mapState =(state) => state;
+
+export default connect(mapState)(WishList);
