f4c33703ddf2e0e62bf57442d6ad5bb600189d46
6StringCode <sixstringcode@gmail.com>
2022-08-18 14:45:10 -0700

    Added Address file to store to create thunks. added to addressBook

--- src/components/Account/AddressForm.js
+++ src/components/Account/AddressForm.js
@@ -2,29 +2,39 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 class AddressForm extends Component {
-    constructor() {
-      super()
-      this.state = {
-        // firstName: this.props.user ? this.props.user.firstName : "",
-        // lastName: this.props.user ? this.props.user.lastName : "",
-        // address: this.props.user ? this.props.user.address : "",
-        // city: this.props.user ? this.props.user.city : "",
-        // state: this.props.user ? this.props.user.state : "",
-        // zipCode: this.props.user ? this.props.user.zipCode : "",
-      }
+  constructor() {
+    super()
+    this.state = {
+      // firstName: this.props.user ? this.props.user.firstName : "",
+      // lastName: this.props.user ? this.props.user.lastName : "",
+      // address: this.props.user ? this.props.user.address : "",
+      // city: this.props.user ? this.props.user.city : "",
+      // state: this.props.user ? this.props.user.state : "",
+      // zipCode: this.props.user ? this.props.user.zipCode : "",
     }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(ev){
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
+  onSubmit(ev){
+    ev.preventDefault();
+    this.props.login(this.state);
+  }
   render() {
+    const { onChange, onSubmit } = this;
     const { firstName, lastName, address, city, state, zipCode } = this.state;
     return (
       <div>
         <h2>Please enter your address in the fields below:</h2>
-        <form>
-            <input placeholder='First Name' name='firstName' required value={ firstName } />
-            <input placeholder='Last Name' name='lastName' required value={ lastName } />
-            <input placeholder='Address' name='address' required value={ address } />
-            <input placeholder='City' name='city' required value={ city } />
-            <input placeholder='State' name='state' required value={ state } />
-            <input placeholder='Postal Code' name='zipCode' required value={ zipCode } />
+        <form onSubmit={ onSubmit }>
+            <input placeholder='First Name' name='firstName' required value={ firstName } onChange={ onChange }/>
+            <input placeholder='Last Name' name='lastName' required value={ lastName } onChange={ onChange }/>
+            <input placeholder='Address' name='address' required value={ address } onChange={ onChange }/>
+            <input placeholder='City' name='city' required value={ city } onChange={ onChange }/>
+            <input placeholder='State' name='state' required value={ state } onChange={ onChange }/>
+            <input placeholder='Postal Code' name='zipCode' required value={ zipCode } onChange={ onChange }/>
             <button>Save</button>
         </form>
       </div>
