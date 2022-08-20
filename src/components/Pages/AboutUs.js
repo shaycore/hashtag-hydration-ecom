8c246a842acc68cc3f1090ff2756fb583c578d1f
Paulina Cole <lina.cole@gmail.com>
2022-08-20 16:28:49 -0400

    updated home page to include carousel, update about me page

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -5,7 +5,15 @@ export const About = (props) => {
 
   return (
     <div>
-        <h1>Contact For Any Queries</h1>
+        <h4>Contact For Any Queries</h4>
+        <p>
+          The only thing we love more than spending
+          time in natural places is helping share that
+          experience with others. That is our purpose
+          and our heartbeat. Whatever you need to help
+          you be outside living your best life, we're in.
+        </p>
+        <img src="https://www.hydroflask.com/media/wysiwyg/hf-2-our-story-dog-above-lake-d.jpg"></img>
     </div>
   );
 };
