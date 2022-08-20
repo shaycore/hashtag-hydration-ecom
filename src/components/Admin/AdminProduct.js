240b34c4f6a66f6e544f8bcbf21868270fcde291
David Shim <dshim0527@gmail.com>
2022-08-20 17:45:51 -0400

    AdminProduct component updated

--- src/components/Admin/AdminProduct.js
+++ src/components/Admin/AdminProduct.js
@@ -2,51 +2,88 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteProduct } from '../../store';
-// import {  } from '../../store';
 
 class _AdminProduct extends Component {
     constructor(){
         super();
         this.state = {
-            product: {},
-            quantity: 1
+            image: '',
+            brand: '',
+            name: '',
+            type: '',
+            description: '',
+            size: '',
+            color: '',
+            price: ''
         };
         this.destroy = this.destroy.bind(this);
     }
     componentDidMount(){
         this.setState({
-            product: this.props.product
+            image: this.props.product.image,
+            brand: this.props.product.brand,
+            name: this.props.product.name,
+            type: this.props.product.type,
+            description: this.props.product.description,
+            size: this.props.product.size,
+            color: this.props.product.color,
+            price: this.props.product.price
+        });
+        this.el.addEventListener('change', ev => {
+            const file = ev.target.files[0];
+            const reader = new FileReader();
+            reader.addEventListener('load', () => {
+                this.setState({ image: reader.result });
+            })
+            reader.readAsDataURL(file);
         })
     }
     componentDidUpdate(prevProps){
         if(!prevProps.product.id && this.props.product.id) {
             this.setState({
-                product: this.props.product
+                image: this.props.product.image,
+                brand: this.props.product.brand,
+                name: this.props.product.name,
+                type: this.props.product.type,
+                description: this.props.product.description,
+                size: this.props.product.size,
+                color: this.props.product.color,
+                price: this.props.product.price
             })
         }
+    }
+    save(ev) {
+        ev.preventDefault();
     }
     destroy(ev) {
         this.props.destroy(this.props.product)
     }
     render(){
-        const { product } = this.state;
-        const { destroy } = this;
+        const { image, brand, name, type, description, size, color, price } = this.state;
+        const { save, destroy } = this;
         return (
             <div id='product'>
                 <Link to={'/admin/products/'}>Return to All Products</Link>
                 <br />
-                <img src={ product.image } alt='Product Image' />
-                <ul>
-                    <li>Brand: { product.brand }</li>
-                    <li>Name: { product.name }</li>
-                    <li>Type: { product.type }</li>
-                    <li>Description: { product.description }</li>
-                    <li>Price: ${ product.price }</li>
-                    <li>Size: { product.size }</li>
-                    <li>Color: { product.color }</li>
-                    <li>Rating: { product.rating }</li>
-
-                </ul>
+                <form onSubmit={ save }>
+                    <img src={ image || '' } alt='Product Image' /><br />
+                    <input type='file' ref={ el => this.el = el }/><br />
+                    <p>Brand</p>
+                    <input placeholder='Brand' value={ brand || '' } onChange={ ev => this.setState({ brand: ev.target.value })}></input><br />
+                    <p>Name</p>
+                    <input placeholder='Name' value={ name || '' } onChange={ ev => this.setState({ name: ev.target.value })}></input><br />
+                    <p>Type</p>
+                    <input placeholder='Type' value={ type || '' } onChange={ ev => this.setState({ type: ev.target.value })}></input><br />
+                    <p>Description</p>
+                    <textarea placeholder='Description' value={ description || '' } onChange={ ev => this.setState({ description: ev.target.value })}></textarea><br />
+                    <p>Size</p>
+                    <input placeholder='Size' value={ size || '' } onChange={ ev => this.setState({ size: ev.target.value })}></input><br />
+                    <p>Color</p>
+                    <input placeholder='Color' value={ color || '' } onChange={ ev => this.setState({ color: ev.target.value })}></input><br />
+                    <p>Price</p>
+                    <input placeholder='Price' value={ price || '' } onChange={ ev => this.setState({ price: ev.target.value })}></input><br />
+                    <button>Save Changes</button>
+                </form>
                 <button onClick={ destroy }>Delete</button>
             </div>
         );
@@ -63,9 +100,6 @@ const mapState = ({ products }, ownProps) => {
 
 const mapDispatch = (dispatch, { history, match }) => {
     return {
-        submit: (obj) => {
-            dispatch(addToCart(obj.product,obj.quantity));
-        },
         destroy: (product) => {
             dispatch(deleteProduct(product, history))
         }
