64ab7d97fa478245998b4c9903d773c0cc0492f1
Paulina Cole <lina.cole@gmail.com>
2022-08-22 22:16:23 -0400

    clear cart working

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
+
+
+
 const cart = (state = { lineItems: [ ] }, action)=> {
   if(action.type === 'SET_CART'){
     state = action.cart;
+  }
+  else if(action.type === 'EMPTY_CART') {
+    state = { lineItems: [ ] };
   }
   return state;
 };
@@ -20,18 +26,11 @@ export const addToCart = (product, diff)=> {
   };
 };
 
-export const clearCart = (state, action)=> {
-  state.cart.lineItems = []
-  return async(dispatch)=> {
-    const response = await axios.get('/api/orders/cart', {
-      headers: {
-        authorization: window.localStorage.getItem('token')
-      }
-    });
-    dispatch({ type: 'SET_CART', cart: response.data });
-
-  };
-};
+export const clearCart = () => {
+  return { 
+    type: 'EMPTY_CART'
+  }
+}
 
 export const fetchCart = ()=> {
   return async(dispatch)=> {
@@ -44,6 +43,16 @@ export const fetchCart = ()=> {
 
   };
 };
+
+export const getCartTotal = () => {
+  const lineItems = getState().cart.lineItems
+  const cartTotal = lineItems &&
+    lineItems.reduce((acc, item) => {
+      acc += item.quantity * item.product?.price;
+      return acc;
+    }, 0);
+  return cartTotal * 1;
+}
 
 
 export default cart;
