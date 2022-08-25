e4ea2d4697d6dc3da4b88c988634b48a93c70c4f
Paulina Cole <lina.cole@gmail.com>
2022-08-25 13:43:00 -0400

    continuing to update home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -117,6 +117,25 @@ return (
                         </div>
                     </div>
                 </div>
+
+    <div className="container-fluid bg-secondary my-5">
+        <div className="row justify-content-md-center py-5 px-xl-5">
+            <div className="col-md-6 col-12 py-5">
+                <div className="text-center mb-2 pb-2">
+                    <h2 className="section-title px-5 mb-3"><span class="bg-secondary px-2">Stay Updated</span></h2>
+                    <p>We are not the hero in this story. You are. We’re inspired by your movement, by the flow of your active lifestyle. We’re in it with you for those long early morning runs, intense workouts, and precious moments with your family on the playground.</p>
+                </div>
+                <form action="">
+                    <div className="input-group">
+                        <input type="text" class="form-control border-white p-4" placeholder="Email Goes Here"/>
+                        <div className="input-group-append">
+                            <button className="btn btn-primary px-4">Subscribe</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
             </div>
         </div>
       </div>
