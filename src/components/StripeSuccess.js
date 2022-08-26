43e95e6aa57f9e79d4c24e5c994e8f05c218bff8
Paulina Cole <lina.cole@gmail.com>
2022-08-26 18:37:59 -0400

    updated success page

--- src/components/StripeSuccess.js
+++ src/components/StripeSuccess.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-//import { connect } from 'react-redux';
-
 class StripeSuccess extends Component {
   render() {
     return (
       <div>
-        <div>Success! Thanks for your order, you'll be hydrated soon!</div>
+        <div className='cart-container'>
+                <div className="container-fluid bg-secondary mb-5">
+                    <div className="d-flex flex-column align-items-center justify-content-center" >
+                        <h1 className="font-weight-semi-bold text-uppercase mb-3">Order Confirmation</h1>
+                            <div className="d-inline-flex">
+                                <p className="m-0 px-2">-</p>
+                                <p className="m-0">Order Confirmation</p>
+                            </div>
+                        </div>
+                    </div>  
+          </div>
+          <div class="text-center mb-4">
+            <h2 class="section-title px-5"><span class="px-2">Thank you for your order!</span></h2>
+        </div>
+        <img src='https://i.etsystatic.com/11696695/r/il/e1101c/3909965980/il_1588xN.3909965980_31h9.jpg'
+        width={'100%'}
+        height={'600px'}/>
       </div>
     )
   }
